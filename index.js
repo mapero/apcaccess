bb6ca7845ecbd2821ba756d077f77a5383a5a1d0
Jochen Scheib <webmaster@mapero.de>
2017-04-30 12:21:30 +0200

    Added CLI

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var ApcAccess = require('./lib/apcaccess');
+var cli = require('cli');
+
+var options = cli.parse({
+  host: ['H', 'Hostname or ip address of the apcupsd nis', 'string', 'localhost'],
+  port: ['p', 'Port of the apcupsd nis', 'int', 3551]
+},
+['status', 'statusJson', 'events']);
 
 var apcaccess = new ApcAccess();
-apcaccess.connect('obelix', 3551)
-.then(function() {return apcaccess.getStatus();})
-.then(function(result) {
-  console.log(result);
-  return apcaccess.getStatusJson();
+apcaccess.connect(options.host, options.port)
+.then(function() {
+  if(cli.command === 'status') return apcaccess.getStatus();
+  else if(cli.command === 'statusJson') return apcaccess.getStatusJson();
+  else if(cli.command === 'events') return apcaccess.getEvents();
 })
 .then(function(result) {
   console.log(result);
   return apcaccess.disconnect();
-})
-.then(function() {
-  console.log('Disconnected!');
 })
 .catch(function(err) {
   console.log(err);
